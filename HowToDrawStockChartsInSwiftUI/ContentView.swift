0df4c6515ba2655e381b8f1c8672cfa81f876209
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-22 11:00:47 +0300

    Initial Commit

---
+++ HowToDrawStockChartsInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowToDrawStockChartsInSwiftUI
+//
+//  Created by ramil on 22.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowToDrawStockChartsInSwiftUI/HowToDrawStockChartsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToDrawStockChartsInSwiftUIApp.swift
+//  HowToDrawStockChartsInSwiftUI
+//
+//  Created by ramil on 22.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToDrawStockChartsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
